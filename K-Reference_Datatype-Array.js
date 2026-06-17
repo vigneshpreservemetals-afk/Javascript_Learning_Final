d35a0b48f1d1fad9261c3dec061ed25d113522c6
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-17 18:50:33 +0530

    Primitive leftout and started reference data type - array

---
+++ K-Reference_Datatype-Array.js
@@ -0,0 +1,25 @@
+
+
+
+// Array is a Group of Related Data Item
+
+let Fruits = ["orange","Grapes","Banana","Papaya","Jackfruit"]
+
+
+console.log("FruitsList===>", Fruits)
+console.log("FruitsList===>", Fruits[0])
+console.log("FruitsList===>", Fruits[1])
+console.log("FruitsList===>", Fruits[2])
+console.log("FruitsList===>", Fruits[3])
+console.log("FruitsList===>", Fruits[4])
+
+// List is a Group of Un-Related Data Item
+
+let EmployeeDetails = ["Vignesh","BE","Mechanical","UI/UX Desiger","Design","Full-Stack"]
+
+console.log("EmployeeName===>",EmployeeDetails[0])
+console.log("DegreeObtained===>",EmployeeDetails[1])
+console.log("CourseDetails===>",EmployeeDetails[2])
+console.log("CurrentProfession===>",EmployeeDetails[3])
+console.log("KeySkills===>",EmployeeDetails[4])
+console.log("AdditionalSkills===>",EmployeeDetails[5])
