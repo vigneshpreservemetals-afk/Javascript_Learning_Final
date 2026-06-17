b56e31be71ebf57abb5a1bdb520842a09be788d6
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-17 17:14:23 +0530

    added primitive types sting

---
+++ F- Primitive_Data_Types.js
@@ -0,0 +1,30 @@
+
+
+// Primitive Date Types -> Stores Single Value
+
+// let is the most preferrable variable (Frequently used)
+
+let name     = "Ram";   //string
+
+let age      = 25;     // Number
+
+let isActive = true;   // Boolean
+
+let x        = null;   // Null
+
+let y;                 // Undefined
+
+
+// Referrence Date Types -> Stores Single Value
+
+// let is the most preferrable variable (Frequently used)
+
+let name     = "Ram";   //string
+
+let age      = 25;     // Number
+
+let isActive = true;   // Boolean
+
+let x        = null;   // Null
+
+let y;                 // Undefined
