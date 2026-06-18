b1f7412e96ac2a488bdfb163e1ba25b2eabdc81c
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-18 08:27:15 +0530

    added object destructure

--- N-Object_Destructure.js
+++ N-Object_Destructure.js
@@ -38,22 +38,21 @@
       "Miro",
     ],
     JobSummary: {
-      "2019-2021": "Preservation Engineer",
-      "2023-Present": "UX Designer",
+      "2019": "Preservation Engineer",
+      "2023": "UX Designer",
     },
-  },
+  };
 
-const {FullName,Height,Skills} = EmployeeDetails
-const {
-    "2019-2021": "Preservation Engineer",  
-    "2023-Present": "UX Designer",
-      } = JobSummary
+const {FullName,Height,Skills,JobSummary} = EmployeeDetails;
+const {"2019": PreviousRole,
+       "2023": Current_Role}              = JobSummary
 
 console.log("FullName===>",FullName)
 console.log("Height===>",Height)
 console.log("Skills===>",Skills)
-console.log(2019-2021)
-
+console.log("JobSummary===>",JobSummary)
+console.log("PreviousRole===>",PreviousRole)
+console.log("CurrentRole===>",Current_Role)
 
 
 
