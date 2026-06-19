6e41fdf5bd249ae45b74f3306f7d4f176ce94088
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-19 07:40:22 +0530

    added and deleted unwanted files

---
+++ A-Arithmetic_Operators.js
@@ -0,0 +1,13 @@
+
+// Example 1
+
+const a = 20;
+
+const b = 10;
+
+console.log(a + b)
+console.log(a - b)
+console.log(a / b)
+console.log(a * b)
+console.log(a % b)     //(Reminder 0 )
+console.log(a ** b)    //( 10 power 20)
--- I-Primitive_Datatypes-Boolean.js
+++
@@ -1,8 +0,0 @@
-
-// Example 1 (Boolean)
-
-let IphoneAvailableOnStock = false;
-
-console.log("Availability===>", IphoneAvailableOnStock)
-
-console.log("typeof===>", typeof IphoneAvailableOnStock)
