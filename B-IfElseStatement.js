48dccdb038740004d68d916d2d11d2b14f0ee045
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-21 17:49:09 +0530

    ifelseif else statement added

---
+++ B-IfElseStatement.js
@@ -0,0 +1,39 @@
+
+
+// Example 1 
+
+let age = 20;
+
+if (age >= 18) {
+
+    console.log("Eligible for voting")
+
+} else
+
+    console.log("Not Eligible")
+
+
+// Example 2
+
+let marks = 95;
+
+if (age >= 75) {
+
+    console.log("A- Grade Marks")
+
+} else
+
+    console.log("Average Marks")
+
+
+// Example 3
+
+let number = 99;
+
+if (number % 2 === 0) {
+
+    console.log("Even Number")
+
+} else
+
+    console.log("Odd Number")
