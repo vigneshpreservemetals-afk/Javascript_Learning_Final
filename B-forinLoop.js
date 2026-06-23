6cd92041ef7bb46211da614dba9da09ca12e005d
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-23 11:49:07 +0530

    added for in loops

---
+++ B-forinLoop.js
@@ -0,0 +1,28 @@
+
+
+// Example 1
+
+// const user = {
+// name: "Vignesh",
+// type: "permanent",
+// Verification: "Verified"
+// }
+
+// for(let key in user){
+//     console.log(key)
+// }
+
+
+// Example 2
+
+const Employee = {
+    firstName : "Vignesh",
+    lastName  : "Baskaran",
+    age:         28
+}
+
+for(let key in Employee){
+    console.log(key, ":", Employee[key] )
+}
+
+
