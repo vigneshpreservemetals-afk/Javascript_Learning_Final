54091c2625aafb46108ab82c33d84047ee1afe71
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-24 15:03:35 +0530

    async await

---
+++ C-Asynchronous_Function.js
@@ -0,0 +1,39 @@
+
+
+// Example 1
+
+// console.log("First Task");
+// console.log("Two Task");
+// console.log("Three Task");
+// console.log("Fifth Task");
+
+// setTimeout(() => {
+
+// for(let i=0; i<=10; i++)
+
+// console.log("Fourth Task")
+    
+// }, 1000);
+
+
+// Example 2
+
+// async function getUser() {
+
+// return 'Vignesh Baskaran'
+
+// }
+
+// const result = getUser();
+
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
